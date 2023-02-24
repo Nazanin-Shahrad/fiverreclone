f862bb0532b53b97aded20820d3710151fa4a9ad
Nazanin-Shahrad <nazanin.shahrad@gmail.com>
2023-02-24 09:32:20 -0500

    all models and Schema completed

--- server/models/gig.model.js
+++ server/models/gig.model.js
@@ -2,10 +2,65 @@ import mongoose from 'mongoose';
 const {Schema} = mongoose;
 
 const GigSchema = new Schema({
-    username:{
+    userId:{
         type: String,
-        required: true,
-        unique:true
+        required :true,
+    },
+    title:{
+        type: String,
+        required :true,
+    },
+    desc:{
+        type: String,
+        required :true,
+    },
+    totalStars:{
+        type: Number,
+        default:0,
+    },
+    starNumber:{
+        type: Number,
+        default:0,
+    },
+    cat:{
+        type: String,
+        required :true,
+    },
+    price:{
+        type: Number,
+        required :true,
+    },
+    cover:{
+        type: String,
+        required :true,
+    },
+    images:{
+        type: [String],
+        required :false,
+    },
+    shortTitle:{
+        type: String,
+        required :true,
+    },
+    shortDesc:{
+        type: String,
+        required :true,
+    },
+    deliveryTime:{
+        type: Number,
+        required :true,
+    },
+    revisionNumber:{
+        type: Number,
+        required :true,
+    },
+    features:{
+        type: [String],
+        required :false,
+    },
+    sales:{
+        type: Number,
+        default :0,
     },
     
     
