f3038ca420f44edcecbc909832067df37147efb1
Nazanin-Shahrad <nazanin.shahrad@gmail.com>
2023-02-25 19:04:55 -0500

    reviews and review component and reviews route added

--- server/routes/review.route.js
+++ server/routes/review.route.js
@@ -1,6 +1,11 @@
 import express from 'express';
+import {createReview , deleteReview , getReview} from '../controllers/review.controller.js'
+import { verifyToken } from '../middleware/jwt';
 
 const router = express.Router();
+router.post("/" ,verifyToken , createReview);
+router.get("/:id" , getReview);
+router.delete("/:id" , deleteReview)
 
 
 
